4b55c505367aac58101bb6ecefdf28e9577c2314
rix <local>
2022-08-04 10:32:50 +1200

    feat: add basic functionality for querying SWAPI resource

--- src/app.mjs
+++ src/app.mjs
@@ -3,6 +3,7 @@ import cors from 'cors';
 import compression from 'compression';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
+import axios from 'axios';
 
 const app = express();
 
@@ -12,8 +13,10 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/resource/:resource', (req, res) => {
-  res.send(`Hello ${req.params.resource}`);
+app.use('/resource/:resource', async (req, res) => {
+  const { resource } = req.params;
+  const { data } = await axios.get(`${process.env.SWAPI_URL}/${resource}`);
+  res.json(data);
 });
 
 app.use('/health', (_, res) => {
