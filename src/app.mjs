d7fdefedea3a85861177a55b614984d634cfeeae
rix <local>
2022-08-04 12:32:29 +1200

    feat: add endpoint `search/all` to do a search across 4 resources

--- src/app.mjs
+++ src/app.mjs
@@ -13,10 +13,35 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/search/all', async (req, res) => {
+  try {
+    const { query } = req.query;
+    const result = await axios.all([
+      axios.get(`${process.env.SWAPI_URL}/people/?search=${query}`),
+      axios.get(`${process.env.SWAPI_URL}/planets/?search=${query}`),
+      axios.get(`${process.env.SWAPI_URL}/species/?search=${query}`),
+      axios.get(`${process.env.SWAPI_URL}/vehicles/?search=${query}`),
+    ]);
+    const data = result.map((response) => response.data.results);
+    res.json({
+      people: result[0].data.results,
+      planets: result[1].data.results,
+      species: result[2].data.results,
+      vehicles: result[3].data.results,
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'There was an internal server error while processing your request. Please try again' });
+  }
+});
+
 app.use('/resource/:resource', async (req, res) => {
-  const { resource } = req.params;
-  const { data } = await axios.get(`${process.env.SWAPI_URL}/${resource}`);
-  res.json(data);
+  try {
+    const { resource } = req.params;
+    const { data } = await axios.get(`${process.env.SWAPI_URL}/${resource}`);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: 'There was an internal server error while processing your request. Please try again' });
+  }
 });
 
 app.use('/health', (_, res) => {
